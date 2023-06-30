d12d2e085e2a5bb3ec794f2522eb8a411b651a37
lynn <lynn@lynndotpy.xyz>
2023-06-30 18:58:08 -0400

    More progress, but metallic broken

--- Ground.gd
+++
@@ -1,9 +0,0 @@
-extends MeshInstance3D
-
-var material = self.get_surface_override_material(0)
-@onready var  player = $"../Player"
-
-func _physics_process(delta):
-	#await player.NOTIFICATION_ENTER_WORLD
-	#print("Ready!")
-	#material.set_shader_parameter("player_pos", player.position)
